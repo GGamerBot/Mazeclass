ded0ac874ca451f72de3f3a2b239dee81510e92f
99ParAnn <pardi.anna99@gmail.com>
2024-05-09 21:23:11 +0200

    Player can move now. it should be impossible to go through walls and move out of bounds.

--- Mazeclass/Maze.cs
+++ Mazeclass/Maze.cs
@@ -7,22 +7,26 @@ using System.Threading.Tasks;
 namespace Mazeclass
 {
     internal class Maze
-    {
+    { 
+        //technically i could get height and width from the matrixes
+        //but it would make things a lot more complicated
         private int height { get; set; }
         private int width { get; set; }
+        //cells matrix will be used to mark the cells: goal + currently standing on + snail trail we made
         public int[,] cells { get; set; }
         //true = there is a wall there. false: there isn't a wall there.
         public bool[,] verticalWalls { get; set; }
         public bool[,] horizontalWalls { get; set; }
         public int playerXCoord { get; set; }
         public int playerYCoord { get; set; }
+
         //first coordinate is horizontal, X, second is vertical, Y
         //they start at the upper left corner
         /*
-         *  0   1   2
-    0   |0,0|1,0|2,0|3,0|
-    1   |0,1|1,1|2,1|3,1|
-    2   |0,2|1,2|2,2|3,2|
+            0   1   2
+    0 _ |0,0|1,0|2,0|3,0|
+    1 _ |0,1|1,1|2,1|3,1|
+        |0,2|1,2|2,2|3,2|
         */
 
         public Maze(bool[,] verticalWalls, bool[,] horizontalWalls)
@@ -33,12 +37,42 @@ namespace Mazeclass
             this.horizontalWalls = horizontalWalls;
             //only the inner walls are represented here
             //the edges are always walls
-            this.height = verticalWalls.GetLength(0)+1;
-            this.width = verticalWalls.GetLength(1)+1;
+            this.height = verticalWalls.GetLength(0);
+            this.width = verticalWalls.GetLength(1);
             playerXCoord = 0;
             playerYCoord = 0;
         }
-        public bool CanIGoThere(bool areWeGoingVertical, bool areWeIncreasingCoord)
+        //using these two bools (areWeGoingVertical,areWeIncreasingCoord)
+        //makes this feel more understandable for me
+        //feel free to revise. maybe keystroke would work better
+        public void MovePlayer(bool areWeGoingVertical, bool areWeIncreasingCoord)
+        {
+            if (CanIGoThere(areWeGoingVertical, areWeIncreasingCoord))
+            {
+                if (areWeGoingVertical && areWeIncreasingCoord)
+                {
+                    playerYCoord++;
+                }
+                else if (areWeGoingVertical)
+                {
+                    playerYCoord--;
+                }
+                else if (areWeIncreasingCoord)
+                {
+                    playerXCoord++;
+                }
+                else
+                {
+                    playerXCoord--;
+                }
+            }
+            //I don't know how to make a better system to show me while debugging
+            //because. i only have a console
+            //wpf should be implemented later
+                Console.WriteLine($"{playerXCoord}, {playerYCoord}");
+        }
+
+        private bool CanIGoThere(bool areWeGoingVertical, bool areWeIncreasingCoord)
         {
             int wallXCoord = this.playerXCoord;
             int wallYCoord = this.playerXCoord;
@@ -51,7 +85,7 @@ namespace Mazeclass
             {
                 wallXCoord = areWeIncreasingCoord ? playerXCoord:playerXCoord-1;                
             }
-            bool isOutOfBounds = wallXCoord < 0 || wallYCoord < 0 || wallXCoord == horizontalWalls.Length || wallYCoord == verticalWalls.Length;
+            bool isOutOfBounds = wallXCoord < 0 || wallYCoord < 0 || wallXCoord == width || wallYCoord == height;
             if (isOutOfBounds)
             {
                 return false;
--- Mazeclass/Program.cs
+++ Mazeclass/Program.cs
@@ -10,19 +10,22 @@ namespace Mazeclass
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("no syntax error");
-            bool[,] walls = new bool[3,3];
+            bool[,] walls = new bool[3, 3];
             for (int i = 0; i < 3; i++)
             {
                 for (int j = 0; j < 3; j++)
                 {
-                    walls[i,j] = false;
+                    walls[i, j] = false;
                 }
             }
             Maze test = new Maze(walls, walls);
-            //horizontal backward:
-            Console.WriteLine(test.CanIGoThere(true, false));
-
+            test.MovePlayer(true, false);
+            test.MovePlayer(false, false);
+            for (int i = 0; i < 4; i++)
+            {
+                test.MovePlayer(true, true);
+                test.MovePlayer(false, true);
+            }
             Console.ReadLine();
         }
     }
