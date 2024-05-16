de6dbbbf8039f0e83a6f30df7d28d14bda207df7
Anna <144335962+Sy-Anna@users.noreply.github.com>
2024-05-16 21:17:37 +0200

    Add files via upload
    Alkalmazás átültetése grafikus felületre

---
+++ Csoportos_projekt--Labirintus_jatek/Labirint-game/Labirint-game/Game.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Labirint_game
+{
+	/// <summary>
+	/// Interaction logic for Game.xaml
+	/// </summary>
+	public partial class Game : Window
+	{
+		public Game()
+		{
+			InitializeComponent();
+		}
+
+		private void inGameSettingsBtn_Click(object sender, RoutedEventArgs e)
+		{
+			MainWindow mainWindow = new MainWindow();
+			this.Hide();
+			mainWindow.Show();
+		}
+	}
+}
---
+++ Csoportos_projekt--Labirintus_jatek/Labirint-game/Labirint-game/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Labirint_game
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		
+		private void settingsBtn_Click(object sender, RoutedEventArgs e)
+		{
+			Settings settings = new Settings();
+			this.Hide();
+			settings.Show();
+		}
+
+		private void startBtn_Click(object sender, RoutedEventArgs e)
+		{
+			Game game = new Game();
+			this.Hide();
+			game.Show();
+		}
+	}
+}
